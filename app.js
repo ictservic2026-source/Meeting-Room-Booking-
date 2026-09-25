f0a920d241b92ac70c9b26ca058d8d99a8507b83
ictservic2026-source <ictservic2026@gmail.com>
2026-09-25 10:55:48 +0700

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@
 // ==========================================================
 // ⚙️ CONFIG
 // ==========================================================
-const API_URL = "https://script.google.com/macros/s/AKfycbw2uog_PgtKfGHVUOq2on2J-Y_IGrVaHgkcypQsDOUaHFxkGO7hG8kJ-kJGAc7XbTeY/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbyFuq4KtiTqJuP_ibd76YrXGBAPd2XG9YMHCFhpsy0EEPVRmR8DMoBK3EA6GLOnaRU/exec";
 
 // ==========================================================
 // 🛠️ SHARED UTILITIES (ใช้ร่วมกันทั้ง 2 หน้า)
